bfa6ad629e86d032344c4cd27cc07290627e18df
Albert <optimisme@localhost.localdomain>
2015-07-07 22:59:40 +0200

    First version

---
+++ README.md
@@ -0,0 +1,52 @@
+# Temperature Map
+Library to draw temperature maps using Canvas/Javascript
+
+### Run
+
+Download this project with:
+
+git clone https://github.com/optimisme/javascript-temperatureMap.git
+
+cd javascript-temperatureMap
+
+Open the example 'index.html' with a web navigator:
+
+temperatureMap.js contains the libary source
+
+...
+
+### Example 0
+
+Animation showing a random temperature map with:
+
+- Low resolution (fast map)
+- High resolution (slow map)
+- Colors by levels
+
+![eg0](https://raw.github.com/optimisme/javascript-temperatureMap/master/captures/eg0.png)
+
+### Example 1
+
+Animation showing a random temperature map with:
+
+- Low resolution (fast map)
+
+![eg1](https://raw.github.com/optimisme/javascript-temperatureMap/master/captures/eg1.png)
+
+### Example 2
+
+Animation showing a random temperature map with:
+
+- High resolution (slow map)
+- Colors by levels
+
+![eg2](https://raw.github.com/optimisme/javascript-temperatureMap/master/captures/eg2.png)
+
+### Example 3
+
+Animation showing a random temperature map with:
+
+- High resolution (slow map)
+- Colors by gradients
+
+![eg3](https://raw.github.com/optimisme/javascript-temperatureMap/master/captures/eg3.png)
---
+++ temperatureMap.js
@@ -0,0 +1,371 @@
+/*global console*/
+/*jslint bitwise: true */
+
+var TemperatureMap = function (ctx) {
+    'use strict';
+    this.ctx = ctx;
+    this.points = [];
+    this.polygon = [];
+};
+
+TemperatureMap.crossProduct = function (o, a, b) {
+    'use strict';
+    return (a.x - o.x) * (b.y - o.y) - (a.y - o.y) * (b.x - o.x);
+};
+
+TemperatureMap.pointInPolygon = function (point, vs) {
+    'use strict';
+    var x = point.x,
+        y = point.y,
+        inside = false,
+        i = 0,
+        j = 0,
+        xi = 0,
+        xj = 0,
+        yi = 0,
+        yj = 0,
+        intersect = false;
+
+    j = vs.length - 1;
+    for (i = 0; i < vs.length; i = i + 1) {
+        xi = vs[i].x;
+        yi = vs[i].y;
+        xj = vs[j].x;
+        yj = vs[j].y;
+
+        intersect = ((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) {
+            inside = !inside;
+        }
+        j = i;
+    }
+
+    return inside;
+};
+
+TemperatureMap.squareDistance = function (p0, p1) {
+    'use strict';
+    var x = Math.abs(p0.x - p1.x),
+        y = Math.abs(p0.y - p1.y);
+
+    return x * x + y * y;
+};
+
+TemperatureMap.hslToRgb = function (h, s, l) {
+    'use strict';
+    var r, g, b, hue2rgb, q, p;
+
+    if (s === 0) {
+        r = g = b = l;
+    } else {
+        hue2rgb = function hue2rgb(p, q, t) {
+            if (t < 0) { t += 1; }
+            if (t > 1) { t -= 1; }
+            if (t < 1 / 6) { return p + (q - p) * 6 * t; }
+            if (t < 1 / 2) { return q; }
+            if (t < 2 / 3) { return p + (q - p) * (2 / 3 - t) * 6; }
+            return p;
+        };
+
+        q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1 / 3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+};
+
+TemperatureMap.prototype.getColor = function (levels, value) {
+    'use strict';
+    var val = value,
+        tmp = 0,
+        lim = 0.55,
+        min = -30,
+        max = 50,
+        dif = max - min,
+        lvs = 25;
+
+    if (val < min) {
+        val = min;
+    }
+    if (val > max) {
+        val = max;
+    }
+
+    tmp = 1 - (1 - lim) - (((val - min) * lim) / dif);
+
+    if (levels) {
+        tmp = Math.round(tmp * lvs) / lvs;
+    }
+
+    return TemperatureMap.hslToRgb(tmp, 1, 0.5);
+};
+
+TemperatureMap.prototype.getPointValue = function (point) {
+    'use strict';
+    var counter = 0,
+        tmp = 0.0,
+        dst = [],
+        inv = [],
+        t = 0.0,
+        b = 0.0,
+        pwr = 2;
+
+    // From : https://en.wikipedia.org/wiki/Inverse_distance_weighting
+
+    if (TemperatureMap.pointInPolygon(point, this.polygon)) {
+        for (counter = 0; counter < this.points.length; counter = counter + 1) {
+            dst[counter] = TemperatureMap.squareDistance(point, this.points[counter]);
+            if (dst[counter] === 0) {
+                return this.points[counter].value;
+            } else {
+                inv[counter] = 1 / Math.pow(dst[counter], pwr);
+                t = t + (inv[counter] * this.points[counter].value);
+                b = b + inv[counter];
+            }
+        }
+        return t / b;
+    } else {
+        return -255;
+    }
+};
+
+TemperatureMap.prototype.setConvexhullPolygon = function (points) {
+    'use strict';
+    var lower = [],
+        upper = [],
+        i = 0;
+
+    points.sort(function (a, b) {
+        return a.x === b.x ? a.y - b.y : a.x - b.x;
+    });
+
+    for (i = 0; i < points.length; i = i + 1) {
+        while (lower.length >= 2 && TemperatureMap.crossProduct(lower[lower.length - 2], lower[lower.length - 1], points[i]) <= 0) {
+            lower.pop();
+        }
+        lower.push(points[i]);
+    }
+
+    for (i = points.length - 1; i >= 0; i = i - 1) {
+        while (upper.length >= 2 && TemperatureMap.crossProduct(upper[upper.length - 2], upper[upper.length - 1], points[i]) <= 0) {
+            upper.pop();
+        }
+        upper.push(points[i]);
+    }
+
+    upper.pop();
+    lower.pop();
+    this.polygon = lower.concat(upper);
+};
+
+TemperatureMap.prototype.setPoints = function (arr, width, height) {
+    'use strict';
+    this.points = arr;
+    this.width = width;
+    this.height = height;
+    this.setConvexhullPolygon(this.points);
+};
+
+TemperatureMap.prototype.setRandomPoints = function (points, width, height) {
+    'use strict';
+    var counter = 0,
+        x = 0,
+        y = 0,
+        v = 0.0,
+        rst = [];
+
+    for (counter = 0; counter < points; counter = counter + 1) {
+
+        x = parseInt((Math.random() * 100000) % width, 10);
+        y = parseInt((Math.random() * 100000) % height, 10);
+        v = (Math.random() * 100) / 2;
+
+        if (Math.random() > 0.5) { v = -v; }
+        if (Math.random() > 0.5) { v = v + 30; }
+
+        rst.push({ x: x, y: y, value: v });
+    }
+
+    this.setPoints(rst, width, height);
+};
+
+TemperatureMap.prototype.drawLow = function (levels, callback) {
+    'use strict';
+    var self = this,
+        ctx = this.ctx,
+        PI2 = 2 * Math.PI,
+        status = { x: 0, y: 0 },
+        recursive = function () {
+            window.requestAnimationFrame(function (timestamp) {
+                var col = [],
+                    cnt = 0,
+                    x = 0,
+                    y = 0,
+                    val = 0.0,
+                    str = '';
+
+                x = status.x;
+                y = status.y;
+                for (cnt = 0; cnt < 50; cnt = cnt + 1) {
+                    val = self.getPointValue({ x: x, y: y });
+                    if (val !== -255) {
+                        col = self.getColor(levels, val);
+
+                        str = 'rgba(' + col[0] + ', ' + col[1] + ', ' + col[2] + ', 0.5)';
+                        ctx.strokeStyle = str;
+                        ctx.lineWidth =  0.4;
+                        ctx.beginPath();
+                        ctx.arc(x, y, 9, 0, PI2, false);
+                        ctx.stroke();
+
+                        str = 'rgba(' + col[0] + ', ' + col[1] + ', ' + col[2] + ', 0.6)';
+                        ctx.strokeStyle = str;
+                        ctx.lineWidth =  0.4;
+                        ctx.beginPath();
+                        ctx.arc(x, y, 7, 0, PI2, false);
+                        ctx.stroke();
+
+                        str = 'rgba(' + col[0] + ', ' + col[1] + ', ' + col[2] + ', 0.7)';
+                        ctx.strokeStyle = str;
+                        ctx.lineWidth =  0.5;
+                        ctx.beginPath();
+                        ctx.arc(x, y, 5, 0, PI2, false);
+                        ctx.stroke();
+
+                        str = 'rgba(' + col[0] + ', ' + col[1] + ', ' + col[2] + ', 0.8)';
+                        ctx.strokeStyle = str;
+                        ctx.lineWidth =  0.5;
+                        ctx.beginPath();
+                        ctx.arc(x, y, 3, 0, PI2, false);
+                        ctx.stroke();
+
+                        str = 'rgba(' + col[0] + ', ' + col[1] + ', ' + col[2] + ', 0.6)';
+                        ctx.strokeStyle = str;
+                        ctx.lineWidth =  0.5;
+                        ctx.beginPath();
+                        ctx.arc(x, y, 1, 0, PI2, false);
+                        ctx.stroke();
+
+                    }
+                    x = x + 10;
+                    if (x >= self.width) {
+                        x = 0;
+                        y = y + 10;
+                    }
+                }
+
+                status.x = x;
+                status.y = y;
+
+                if (y < self.height) {
+                    recursive();
+                } else if (typeof callback === 'function') {
+                    callback();
+                }
+            });
+        };
+
+    recursive();
+};
+
+TemperatureMap.prototype.drawFull = function (levels, callback) {
+    'use strict';
+    var self = this,
+        ctx = this.ctx,
+        img = this.ctx.getImageData(0, 0, self.width, self.height),
+        status = { x: 0, y: 0, type: 'even' },
+        recursive = function () {
+            window.requestAnimationFrame(function (timestamp) {
+                var col = [],
+                    cnt = 0,
+                    idx = 0,
+                    x = 0,
+                    y = 0,
+                    w = 0,
+                    val = 0.0;
+
+                x = status.x;
+                y = status.y;
+                w = self.width * 4;
+
+                for (cnt = 0; cnt < 500; cnt = cnt + 1) {
+                    val = self.getPointValue({ x: x, y: y });
+                    idx = x * 4 + y * w;
+                    if (val !== -255) {
+                        col = self.getColor(levels, val);
+                        img.data[idx] = col[0];
+                        img.data[idx + 1] = col[1];
+                        img.data[idx + 2] = col[2];
+                        img.data[idx + 3] = 128;
+                    } else {
+                        img.data[idx] = 0;
+                        img.data[idx + 1] = 0;
+                        img.data[idx + 2] = 0;
+                        img.data[idx + 3] = 0;
+                    }
+                    x = x + 1;
+                    if (x >= self.width) {
+                        x = 0;
+                        y = y + 2;
+                    }
+                }
+
+                ctx.putImageData(img, 0, 0);
+                status.x = x;
+                status.y = y;
+
+                if (y < self.height) {
+                    recursive();
+                } else if (status.type === 'even') {
+                    status.type = 'odd';
+                    status.y = 1;
+                    recursive();
+                } else if (typeof callback === 'function') {
+                    callback();
+                }
+            });
+        };
+
+    recursive();
+};
+
+TemperatureMap.prototype.drawPoints = function (callback) {
+    'use strict';
+    var self = this,
+        PI2 = 2 * Math.PI,
+        ctx = this.ctx;
+    window.requestAnimationFrame(function (timestamp) {
+        var col = [],
+            idx = 0,
+            pnt;
+
+        for (idx = 0; idx < self.points.length; idx = idx + 1) {
+            pnt = self.points[idx];
+
+            col = self.getColor(false, pnt.value);
+
+            ctx.fillStyle = 'rgba(255, 255, 255, 128)';
+            ctx.beginPath();
+            ctx.arc(pnt.x, pnt.y, 8, 0, PI2, false);
+            ctx.fill();
+
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = 'rgb(' + col[0] + ', ' + col[1] + ', ' + col[2] + ')';
+            ctx.beginPath();
+            ctx.arc(pnt.x, pnt.y, 8, 0, PI2, false);
+            ctx.stroke();
+
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'middle';
+            ctx.fillStyle = 'rgb(0, 0, 0)';
+            ctx.fillText(Math.round(pnt.value), pnt.x, pnt.y);
+        }
+
+        if (typeof callback === 'function') {
+            callback();
+        }
+    });
+};
